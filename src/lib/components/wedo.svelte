0fec0f1d195f6120f344dea31d246e037a8c0434
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-04-29 13:41:12 +0000

    updated landing page

--- src/lib/components/wedo.svelte
+++ src/lib/components/wedo.svelte
@@ -1,28 +1,30 @@
 <script lang="ts">
 	import Thingswedo from './thingswedo.svelte';
-    import * as Icon from 'svelte-google-materialdesign-icons';
-    
-    
+	import * as Icon from 'svelte-google-materialdesign-icons';
 </script>
 
-<div class="bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-amber-600 via-transparent to-transparent flex w-full items-center justify-between px-10 flex-col md:flex-row p-5">
-	<div class=" md:w-1/2 text-center gap-y-3 md:gap-y-0 text-[25px] md:text-[40px]">We Do It For People In Need</div>
-	<div class="md:w-1/2 flex flex-row gap-5 justify-center">
+<div
+	class="flex w-full gap-3 md:gap-0 flex-col items-center justify-between bg-[conic-gradient(at_top_left,_var(--tw-gradient-stops))] from-amber-600 via-transparent to-transparent p-5 px-10 md:flex-row"
+>
+	<div class="text-center md:w-1/2 text-xl md:text-[40px]">
+		We Do It For People In Need
+	</div>
+	<div class="grid grid-cols-2 md:flex h-full md:flex-row justify-center gap-5  md:w-1/2">
 		<Thingswedo>
-            <span slot="icon"><Icon.Medical_services variation="filled" class=""/></span>
-            <span slot="description">Medicine <br> Help</span>
-        </Thingswedo>
-        <Thingswedo>
-            <span slot="icon"><Icon.Build class=""/></span>
-            <span slot="description">Build <br>Create</span>
-        </Thingswedo>
-        <Thingswedo>
-            <span slot="icon"><Icon.Fastfood class=""/></span>
-            <span slot="description">Food <br>Help</span>
-        </Thingswedo>
-        <Thingswedo>
-            <span slot="icon"><Icon.Sports class=""/></span>
-            <span slot="description">Sport <br>Enguagement</span>
-        </Thingswedo>
+			<span slot="icon"><Icon.Medical_services variation="filled" class="" /></span>
+			<span slot="description">Medicine Help</span>
+		</Thingswedo>
+		<Thingswedo>
+			<span slot="icon"><Icon.Build class="" /></span>
+			<span slot="description">Build Create</span>
+		</Thingswedo>
+		<Thingswedo>
+			<span slot="icon"><Icon.Fastfood class="" /></span>
+			<span slot="description">Food Help</span>
+		</Thingswedo>
+		<Thingswedo>
+			<span slot="icon"><Icon.Sports class="" /></span>
+			<span slot="description">Sport Enguagement</span>
+		</Thingswedo>
 	</div>
 </div>
