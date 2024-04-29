faf9fd34cedce897fcd03406789bdd4557c4ebb7
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-04-29 22:59:51 +0000

    updated landing page

--- src/lib/components/showcase.svelte
+++ src/lib/components/showcase.svelte
@@ -2,35 +2,43 @@
 	import together from '$lib/images/children-5833685_1280.jpg';
 	import Button from '$lib/components/button.svelte';
 	export let order: string = 'last';
+	export let image:string = together;
+	export let btn:boolean = true;
 </script>
 
 <div class="flex flex-col w-full items-center justify-between gap-5 p-3 text-gray-300 md:flex-row md:px-10 my-5">
     <!-- text below  -->
 	<div class="w-full md:w-1/2 space-y-3">
+		<!-- title slot -->
 		<p class=" text-center hidden md:block font-bold text-amber-600 text-[20px] md:text-[40px]">
-			Helping Each Other Can Make World Better
+			<slot name="title">Helping Each Other Can Make World Better</slot>
 		</p>
         <p class="md:hidden text-center font-bold text-amber-600 text-[20px]">
-			Helping Each Other Can Make World Better
+			<slot name="title">Helping Each Other Can Make World Better</slot>
 		</p>
 		<p class="text-lg text-center ">
-			While we take a moment to appreciate how far we've come, we also look forward to the path
+			<!-- description slot here -->
+			<slot name="description">While we take a moment to appreciate how far we've come, we also look forward to the path
 			ahead. There are still many lives waiting for a beacon of hope, and with your continued
 			support, we can reach even further. Each contribution empowers us to expand our outreach and
-			deepen our impact.
+			deepen our impact.</slot>
 		</p>
-		<div class=" hidden space-x-5 md:block">
+
+		{#if btn}
+<div class=" hidden space-x-5 md:block">
 			<Button btnstyle="rounded-lg bg-amber-600 p-2 font-bold text-slate-900 hover:bg-amber-500">
 				Our Mission
 			</Button>
 			<Button
 				btnstyle="rounded-lg border border-amber-600 p-2 font-bold hover:border-gray-300 hover:text-amber-500"
 			>
-				Our Vision
+				Our Story
 			</Button>
 		</div>
+		{/if}
+		
 	</div>
 
     <!-- image below -->
-	<div class="w-full md:w-1/2 order-{order}"><img class="rounded-xl" src={together} alt="together" /></div>
+	<div class="w-full md:w-1/2 order-{order}"><img class="rounded-xl" src={image} alt="together" /></div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,46 @@
 <script>
-    import Topnav from "$lib/components/topnav.svelte";
-    import Heroimage from "$lib/components/heroimage.svelte";
-    import Showcase from "$lib/components/showcase.svelte";
-	import Wedo from "$lib/components/wedo.svelte";
-	import Ourmission from "$lib/components/ourmission.svelte";
-
-
-
+	import Topnav from '$lib/components/topnav.svelte';
+	import Heroimage from '$lib/components/heroimage.svelte';
+	import Showcase from '$lib/components/showcase.svelte';
+	import Wedo from '$lib/components/wedo.svelte';
+	import Ourmission from '$lib/components/ourmission.svelte';
+	import successstory from '$lib/images/against-light-7572922_1280.jpg';
 </script>
-<div class="bg-slate-900 h-screen w-screen text-white overflow-x-hidden font-serif">
-    <!-- topnav below -->
-    <Topnav/>
-    <!-- topnav ends -->
 
-    <!-- heroimage below -->
-    <Heroimage/>
-    <!-- heroimage ends -->
+<div class="h-screen w-screen overflow-x-hidden bg-slate-900 font-serif text-white">
+	<!-- topnav below -->
+	<Topnav />
+	<!-- topnav ends -->
 
-    <!-- showcase below -->
-    <Showcase/>
-    <!-- showcase ends -->
+	<!-- heroimage below -->
+	<Heroimage />
+	<!-- heroimage ends -->
 
-    <!-- wedo below -->
-    <Wedo/>
-    <!-- wedo ends -->
+	<!-- showcase below -->
+	<Showcase />
+	<!-- showcase ends -->
 
-    <!-- Our Mission below -->
-    <Ourmission/>
-    <!-- Our Mission ends -->
+	<!-- wedo below -->
+	<Wedo />
+	<!-- wedo ends -->
+
+	<!-- Our Mission below -->
+	<Ourmission />
+	<!-- Our Mission ends -->
+
+	<!-- Our Success Story below -->
+	<Showcase order="first" image={successstory} btn={false}>
+		<div slot="title">Our Success Story</div>
+		<div slot="description">
+            "Since our inception, we have seen countless lives transformed through our collective efforts.
+			In the heart of Africa, we've ignited a spark of change one child, one game, one meal at a
+			time. Our sports programs have cultivated discipline and joy, nourishing both the body and
+			spirit of the young athletes. Beyond the field, our initiative has clothed, fed, and healed,
+			extending a lifeline to those in dire need. These victories aren't just ours, they belong to
+			every supporter who believed in the cause. Together, we have sown seeds of hope that will
+			bloom for generations to come."
+			
+		</div>
+	</Showcase>
+	<!-- Our Success Story ends -->
 </div>
