6f7e9f3ffdb3ee58ce9109a5806d566c7d9b6783
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-04-29 15:14:48 +0000

    Update thingswedo.svelte

--- src/lib/components/thingswedo.svelte
+++ src/lib/components/thingswedo.svelte
@@ -1,4 +1,4 @@
-<div class="flex md:flex-row justify-center items-center text-center gap-1 border-l-2 border-slate-900 pl-2 h-full ">
+<div class="flex md:flex-row justify-center items-center text-center gap-1 border-l-2 border-slate-900 pl-2 ">
 <div><slot name="icon">Icon</slot></div>
 <div><slot name="description">Description</slot></div>
 </div>
