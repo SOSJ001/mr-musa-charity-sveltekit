7b1e5f9602fcc5da321938d2716609b7693d5c80
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-04-30 07:20:48 +0000

    updated landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import Wedo from '$lib/components/wedo.svelte';
 	import Ourmission from '$lib/components/ourmission.svelte';
 	import successstory from '$lib/images/against-light-7572922_1280.jpg';
+	import news from '$lib/images/africa-news.jpg';
 </script>
 
 <div class="h-screen w-screen overflow-x-hidden bg-slate-900 font-serif text-white">
@@ -32,15 +33,29 @@
 	<Showcase order="first" image={successstory} btn={false}>
 		<div slot="title">Our Success Story</div>
 		<div slot="description">
-            "Since our inception, we have seen countless lives transformed through our collective efforts.
+			"Since our inception, we have seen countless lives transformed through our collective efforts.
 			In the heart of Africa, we've ignited a spark of change one child, one game, one meal at a
 			time. Our sports programs have cultivated discipline and joy, nourishing both the body and
 			spirit of the young athletes. Beyond the field, our initiative has clothed, fed, and healed,
 			extending a lifeline to those in dire need. These victories aren't just ours, they belong to
 			every supporter who believed in the cause. Together, we have sown seeds of hope that will
 			bloom for generations to come."
-			
 		</div>
 	</Showcase>
 	<!-- Our Success Story ends -->
+
+	<!-- News  -->
+	<Showcase image={news} btn={false}>
+		<div slot="title">News And Resources</div>
+		<div slot="description">
+			A group of resilient children gather, their faces telling stories
+			of survival and hope. Despite the challenging circumstances they face daily, their spirits
+			remain unbroken. Through local initiatives, these young souls are not only finding solace in
+			sports and community activities but are also receiving essential support such as food,
+			clothing, and medical care. These interventions aim to provide these children with the tools
+			they need to build a promising future, proving that even in the toughest conditions, hope can
+			thrive.
+		</div>
+	</Showcase>
+	<!-- Our News ends -->
 </div>
